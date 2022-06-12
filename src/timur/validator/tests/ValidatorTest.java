7037ef73144ae4c002615f391ad80d014e6dc713
TimurMP <titimurmamyshev@gmail.commur>
2022-06-13 01:00:17 +0300

    Added checkDateFormatEU

--- src/timur/validator/tests/ValidatorTest.java
+++ src/timur/validator/tests/ValidatorTest.java
@@ -1,4 +1,49 @@
+package timur.validator.tests;
+
+import org.junit.jupiter.api.Test;
+import timur.validator.tools.Validator;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ValidatorTest {
-  
+
+    @Test
+    void checkCreditCard() {
+        assertTrue(Validator.checkCreditCard("12345678"));
+        assertTrue(Validator.checkCreditCard("1234567890"));
+        assertTrue(Validator.checkCreditCard("1234567890987654"));
+        assertFalse(Validator.checkCreditCard("12345678909876541234567890987654"));
+        assertFalse(Validator.checkCreditCard("123456aasddss545"));
+
+
+
+    }
+
+    @Test
+    void checkDateFormatEU() {
+        //Format: DD.MM.YYYY
+        assertTrue(Validator.checkDateFormatEU("16.02.1988"));
+        assertTrue(Validator.checkDateFormatEU("01.01.2001"));
+        assertTrue(Validator.checkDateFormatEU("12.12.2999"));
+        assertFalse(Validator.checkDateFormatEU("00.00.0000"));
+        assertFalse(Validator.checkDateFormatEU("32.03.1995"));
+        assertFalse(Validator.checkDateFormatEU("11.15.2021"));
+        assertFalse(Validator.checkDateFormatEU("03.03.3033"));
+
+
+
+
+    }
+
+    @Test
+    void checkDateFormatUS() {
+    }
+
+    @Test
+    void checkPhoneNumber() {
+    }
+
+    @Test
+    void checkLessEquals255() {
+    }
 }
--- src/timur/validator/tools/Validator.java
+++ src/timur/validator/tools/Validator.java
@@ -1,2 +1,26 @@
-package timur.validator.tools;public class Validator {
+package timur.validator.tools;
+
+public class Validator  {
+    public static boolean checkCreditCard(String str) {
+        String pattern = "\\d{8,16}";
+        return str.matches(pattern);
+    }
+    public static boolean checkDateFormatEU(String str) {
+        //Format: DD.MM.YYYY
+        String pattern = "([0-3][0-1]|[0-2][0-9])\\.([0-1][0-2]|0[0-9])\\.([1-2][0-9]{3})";
+        return str.matches(pattern);
+    }
+    public static boolean checkDateFormatUS(String str) {
+        //TODO
+        return false;
+    }
+    public static boolean checkPhoneNumber(String str) {
+        //TODO
+        return false;
+    }
+    public static boolean checkLessEquals255(String str) {
+        //TODO
+        return false;
+    }
+
 }
